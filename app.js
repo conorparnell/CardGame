1d0b4079a360f99badb2015c4b4222a9c8f7fbf7
Conor Parnell <conorparnell@gmail.com>
2023-01-14 22:07:54 -0500

    working player beats logic

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let userPoints = 0;
 let userHype = 0;
 let userFear = 0;
+let userBeats = 3;
+
 
 const deck = ["Lil Lindy", "Lil Lindy", "Lil Lindy", "Gavotte Trot", "Gavotte Trot", "Flip Floss", "Takey Tap Tap", "One Step Pep", "Rhythm & Grow", "Rig-A-Jig"];
 
@@ -9,6 +11,7 @@ let drawingCards = deck;
 
 //New cards:
 function startTurn() {
+    userBeats = 3;
     hand = [];
     drawingCards = deck;
     for (i = 0; i < 3; i++) {
@@ -28,67 +31,96 @@ function startTurn() {
 
 //Lil Lindy
 function lindy() {
-    let points = 2;
-    points += userHype;
-    points += userFear;
+    if (checkCost(1)) {
+        let points = 2;
+        points += userHype;
+        points += userFear;
 
-    if (points <= 0) {
-        points = 0;
+        if (points <= 0) {
+            points = 0;
+        }
+        userPoints += points;
+        console.log(`You gain ${points} points. You now have ${userPoints} points!`);
+        checkGame(userPoints);
     }
-    userPoints += points;
-    console.log(`You gain ${points} points. You now have ${userPoints} points!`);
-    checkGame(userPoints);
 }
 
 //Gavotte Trot
 function gavotte() {
-    let points = 4;
-    points += userHype;
-    points += userFear;
+    if (checkCost(2)) {
+        let points = 4;
+        points += userHype;
+        points += userFear;
 
-    if (points <= 0) {
-        points = 0;
+        if (points <= 0) {
+            points = 0;
+        }
+        userPoints += points;
+        console.log(`You gain ${points} points. You now have ${userPoints} points!`);
+        checkGame(userPoints);
     }
-    userPoints += points;
-    console.log(`You gain ${points} points. You now have ${userPoints} points!`);
-    checkGame(userPoints);
 }
 
 //Flip Floss
 function flip() {
-    let points = 7;
-    points += userHype;
-    points += userFear;
+    if (checkCost(3)) {
+        let points = 7;
+        points += userHype;
+        points += userFear;
 
-    if (points <= 0) {
-        points = 0;
+        if (points <= 0) {
+            points = 0;
+        }
+        userPoints += points;
+        console.log(`You gain ${points} points. You now have ${userPoints} points!`);
+        checkGame(userPoints);
     }
-    userPoints += points;
-    console.log(`You gain ${points} points. You now have ${userPoints} points!`);
-    checkGame(userPoints);
 }
 
 //One Step Pep
 function pep() {
-    userHype++;
-    console.log(`You gain 1 hype. You now have ${userHype} hype!`);
+    if (checkCost(2)) {
+        userHype++;
+        console.log(`You gain 1 hype. You now have ${userHype} hype!`);
+    }
 }
 
 //Rig-A-Jig
 function rig() {
-    if (drawingCards <= 0) {
-        console.log("No cards left to draw :(")
-    } else {
-        console.log("You draw one card.");
-        let draw = Math.floor(Math.random() * drawingCards.length);
-        hand[i] = drawingCards[draw];
-        console.log(`${drawingCards[draw]} added to hand.`);
-        drawingCards.splice(draw, 1);
+    if (checkCost(1)) {
+        if (drawingCards <= 0) {
+            console.log("No cards left to draw :(")
+        } else {
+            console.log("You draw one card.");
+            let draw = Math.floor(Math.random() * drawingCards.length);
+            hand[i] = drawingCards[draw];
+            console.log(`${drawingCards[draw]} added to hand.`);
+            drawingCards.splice(draw, 1);
+        }
     }
 }
+
+//Rhythm & Grow
+function rhythm() {
+    checkCost(0);
+    userBeats++;
+    console.log(`You gain 1 beat, you now have ${userBeats} beats`)
+}
+
+//helper functions
 
 function checkGame(userPoints) {
     if (userPoints >= 20) {
         console.log("You win!!!");
+    }
+}
+
+function checkCost(cardCost) {
+    if (cardCost > userBeats) {
+        console.log("Not enough beats to play that card!");
+        return false;
+    } else {
+        userBeats -= cardCost;
+        return true;
     }
 }
