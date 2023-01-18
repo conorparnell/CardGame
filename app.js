623bdedecc59aaee592afd98072e38c0c02c7ab7
Conor Parnell <conorparnell@gmail.com>
2023-01-18 10:37:02 -0500

    score and brawn counter working, game cards all working for user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const startingHandSize = 4;
 const startingBrawn = 4;
 const winningScore = 30;
 let userTurn = true; //boolean for whose turn it is. user = true, rival = false;
+let turnOver = false; //boolean for if the user is done with their turn
 
 //USER setup
 let userBones = 0;
@@ -18,8 +19,8 @@ let rivalFear = 0;
 
 //initialize scorekeeping
 window.addEventListener('DOMContentLoaded', (event) => {
-    updateUserScore(0);
-    userRivalScore(0);
+    updateScore();
+    updateBrawn();
 });
 
 
@@ -428,7 +429,7 @@ let rivalHand = [];
 
 //GAME LOOP:
 //while (gameOver) {
-   // while (userTurn) {
+  // while (userTurn) {
         //User Turn
         //Brawn refilled
         refillBrawn();
@@ -436,24 +437,35 @@ let rivalHand = [];
         drawCards(startingHandSize);
         //display hand
         displayHand();
+        //while(!turnOver){
+            //do nothing, play cards, then hit pass turn
+        //}
         //play cards
         //endTurn -> remove all card elements, flush hand, refill remaining deck
-  //  }
+   //}
     //pass turn -> userTurn = false
     //rival brawn refilled
     //rival draws cards
     //rival plays cards
     //pass turn -> userTurn = true
-    userTurn = true;
+    //userTurn = true;
+    //turnOver = false;
 //}
-//
+
 
 function passTurn(){
     userTurn = false;
+    turnOver = true;
     let allCards = document.querySelectorAll(".card");
     allCards.forEach(element => element.remove());
     userHand = [];
     userRemainingDeck = Array.from(userDeck);
+}
+
+function refreshCardText(){
+    let allCards = document.querySelectorAll(".card");
+    allCards.forEach(element => element.remove());
+    displayHand();
 }
 
 //logic to display a card on the gameboard
@@ -502,10 +514,14 @@ function displayCard(card) {
     newCard.addEventListener('click', (e) => {
         if (checkCost(card.cardCost)) {
             playCard(card.cardId);
+            payCost(card.cardCost);
+            updateScore();
+            updateBrawn();
             removeCard(e);
             if (userTurn) {
                 let cardIndex = userHand.indexOf(card);
                 userHand.splice(cardIndex, 1);
+                refreshCardText();
             }
         }
     });
@@ -532,6 +548,14 @@ function checkCost(cost) {
     }
 }
 
+function payCost(cost){
+    if (userTurn){
+        userBrawn -= cost;
+        console.log(`${userBrawn} brawn left`);
+    } else {
+        rivalBrawn -= cost;
+    }
+}
 
 
 function removeCard(e) {
@@ -544,7 +568,6 @@ function removeCard(e) {
 
 //displays each card in hand
 function displayHand() {
-
     for (i = 0; i < userHand.length; i++) {
         displayCard(userHand[i]);
     }
@@ -582,8 +605,10 @@ function drawCards(num) {
 function refillBrawn() {
     if (userTurn) {
         userBrawn = startingBrawn;
+        updateBrawn();
     } else {
         rivalBrawn = startingBrawn;
+        updateBrawn();
     }
 }
 
@@ -635,40 +660,6 @@ function calculateBones(num) {
 /*
 
 
-const deck = ["Lil Lindy", "Lil Lindy", "Lil Lindy", "Gavotte Trot", "Gavotte Trot", "Flip Floss", "Takey Tap Tap", "One Step Pep", "Rhythm & Grow", "Rig-A-Jig"];
-const rivalDeck = ["Twizzle Sizzle", "Twizzle Sizzle", "Twizzle Sizzle", "Dainty Dip", "Dainty Dip", "Glissade Parade", "Mooching Mamba", "Cheer Squad"];
-
-let hand = [];
-let drawingCards = Array.from(deck);
-
-let rivalHand = [];
-let rivalCards = Array.from(deck);
-
-//New cards:
-function startTurn() {
-    const previousCards = document.querySelectorAll(".card");
-    previousCards.forEach(element => element.remove());
-    userBeats = 4;
-    hand = [];
-    drawingCards = Array.from(deck);
-    for (i = 0; i < 3; i++) {
-        let draw = Math.floor(Math.random() * drawingCards.length);
-        hand[i] = drawingCards[draw];
-        let cardPulled = drawingCards[draw];
-        console.log(`${drawingCards[draw]} added to hand.`);
-        let newCard = document.createElement("div");
-        newCard.classList.add("card");
-        newCard.innerText = `${drawingCards[draw]}`;
-        newCard.addEventListener('click', () => {
-            playCard(cardPulled);
-        });
-        newCard.addEventListener('click', removeCard);
-        const gameBoard = document.getElementById("gameboard");
-        gameBoard.appendChild(newCard);
-        drawingCards.splice(draw, 1);
-    }
-}
-
 //Enemy turn:
 function rivalTurn() {
     rivalBeats = 4;
@@ -684,231 +675,6 @@ function rivalTurn() {
 }
 
 
-
-//Player Card Functions:
-
-function playCard(cardName) {
-    switch (cardName) {
-        case "Lil Lindy":
-            lindy();
-            break;
-        case "Gavotte Trot":
-            gavotte();
-            break;
-        case "Flip Floss":
-            flip();
-            break;
-        case "Takey Tap Tap":
-            takey();
-            break;
-        case "One Step Pep":
-            pep();
-            break;
-        case "Rhythm & Grow":
-            rhythm();
-            break;
-        case "Rig-A-Jig":
-            rig();
-            break;
-    }
-}
-
-//Lil Lindy
-function lindy() {
-    if (checkCost(1)) {
-        let points = 2;
-        points += userHype;
-        points -= userFear;
-
-        if (points <= 0) {
-            points = 0;
-        }
-        console.log(`You gain ${points} points. You now have ${userPoints} points!`);
-        updatePlayerScore(points);
-        checkGame();
-    }
-}
-
-//Gavotte Trot
-function gavotte() {
-    if (checkCost(2)) {
-        let points = 4;
-        points += userHype;
-        points -= userFear;
-
-        if (points <= 0) {
-            points = 0;
-        }
-        console.log(`You gain ${points} points. You now have ${userPoints} points!`);
-        updatePlayerScore(points);
-        checkGame();
-    }
-}
-
-//Flip Floss
-function flip() {
-    if (checkCost(3)) {
-        let points = 7;
-        points += userHype;
-        points -= userFear;
-
-        if (points <= 0) {
-            points = 0;
-        }
-        userPoints += points;
-        console.log(`You gain ${points} points. You now have ${userPoints} points!`);
-        checkGame();
-    }
-}
-
-//Takey Tap Tap
-function takey() {
-    if (checkCost(3)) {
-        let points = 3;
-        points += userHype;
-        points -= userFear;
-
-        if (points <= 0) {
-            points = 0;
-        }
-
-        if (rivalPoints < points) {
-            points = rivalPoints;
-        }
-
-        userPoints += points;
-        rivalPoints -= points;
-        console.log(`You steal ${points} points. You now have ${userPoints} points!`);
-        checkGame();
-    }
-}
-
-//One Step Pep
-function pep() {
-    if (checkCost(2)) {
-        userHype++;
-        console.log(`You gain 1 hype. You now have ${userHype} hype!`);
-    }
-}
-
-//Rig-A-Jig
-function rig() {
-    if (checkCost(1)) {
-        if (drawingCards <= 0) {
-            console.log("No cards left to draw :(")
-        } else {
-            console.log("You draw one card.");
-            let draw = Math.floor(Math.random() * drawingCards.length);
-            hand[i] = drawingCards[draw];
-            console.log(`${drawingCards[draw]} added to hand.`);
-            drawingCards.splice(draw, 1);
-        }
-    }
-}
-
-//Rhythm & Grow
-function rhythm() {
-    checkCost(0);
-    userBeats++;
-    console.log(`You gain 1 beat, you now have ${userBeats} beats`)
-}
-
-
-//Rival Card Functions
-
-//Twizzle Sizzle
-function twizzle() {
-    rivalBeats -= 1;
-    let points = 1;
-    points += rivalHype;
-    points -= rivalFear;
-
-    if (points <= 0) {
-        points = 0;
-    }
-    rivalPoints += points;
-    console.log(`Your rival gains ${points} point. Rival now has ${rivalPoints} points!`);
-    checkGame();
-}
-
-//Dainty Dip
-function dainty() {
-    rivalBeats -= 2;
-    let points = 3;
-    points += rivalHype;
-    points -= rivalFear;
-
-    if (points <= 0) {
-        points = 0;
-    }
-    rivalPoints += points;
-    console.log(`Your rival gains ${points} point. Rival now has ${rivalPoints} points!`);
-    checkGame();
-}
-
-//Glissade Parade
-function glissade() {
-    rivalBeats -= 3;
-    let points = 5;
-    points += rivalHype;
-    points -= rivalFear;
-
-    if (points <= 0) {
-        points = 0;
-    }
-    rivalPoints += points;
-    console.log(`Your rival gains ${points} point. Rival now has ${rivalPoints} points!`);
-    checkGame();
-
-}
-
-//Mooching Mamba
-function mooching() {
-    rivalBeats -= 4;
-    let points = 3;
-    points += rivalHype;
-    points -= rivalFear;
-
-    if (points <= 0) {
-        points = 0;
-    }
-
-    if (userPoints < points) {
-        points = userPoints;
-    }
-
-    userPoints -= points;
-    rivalPoints += points;
-    console.log(`Your rival steals ${points} points. Rival now has ${rivalPoints} points!`);
-    checkGame();
-}
-
-//Cheer Squad
-function cheer() {
-    rivalBeats -= 2;
-    rivalHype += 1;
-    console.log(`Rival gains 1 hype. Rival now has ${rivalHype} hype!`)
-}
-
-//helper functions
-
-function checkGame() {
-    if (userPoints >= 20) {
-        console.log("You win!!!");
-    }
-    if (rivalPoints >= 20) {
-        console.log("You lose :(");
-    }
-}
-
-function checkCost(cardCost) {
-    if (cardCost > userBeats) {
-        console.log("Not enough beats to play that card!");
-        return false;
-    } else {
-        return true;
-    }
-}
 
 //Enemy turn
 
@@ -953,31 +719,38 @@ function rivalPlay() {
 
     }
 }
-
-function displayCards() {
-    //
-}
-
-// while (true) {
-//     startTurn();
-//     rivalTurn();
-// }
-
 */
 
-
 const orangeDiv = document.getElementById("something");
-orangeDiv.addEventListener('click', passTurn);
+orangeDiv.addEventListener('click', restartTurn);
 //const greenDiv = document.getElementById("options");
 //greenDiv.addEventListener('click', rivalTurn);
 //const greyDiv = document.getElementById("placeholder");
 //greyDiv.addEventListener('click', )
 
+function restartTurn(){
+    let allCards = document.querySelectorAll(".card");
+    allCards.forEach(element => element.remove());
+    userHand = [];
+    userRemainingDeck = Array.from(userDeck);
+    refillBrawn();
+    drawCards(startingHandSize);
+    displayHand();
 
-function updateUserScore(points){
-    const playerScore = document.getElementById('player-score');
-    const playerScoreBar = document.getElementById('playerScore');
-    userBones = userBones + points;
-    playerScore.innerText = `${userBones}`;
-    playerScoreBar.setAttribute('value', `${userBones}`);
+}
+
+function updateScore(){
+    const userScore = document.getElementById('user-score');
+    const userScoreBar = document.getElementById('user-score-bar');
+    userScore.innerText = `${userBones}`;
+    userScoreBar.setAttribute('value', `${userBones}`);
+
+    //add logic for rival
+}
+
+function updateBrawn(){
+    const userBrawnAmount = document.getElementById('user-brawn');
+    //const rivalBrawnAmount = document.getElementById('rival-brawn');
+    userBrawnAmount.innerText = `${userBrawn}`;
+    //rivalBrawnAmount.innerText = `${rivalBrawn}`;
 }
