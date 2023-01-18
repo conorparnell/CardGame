3a58b130a12108b92e6730d1ba918ae86c97de06
Conor Parnell <conorparnell@gmail.com>
2023-01-17 19:27:58 -0500

    huge overhaul of logic, nearly done

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-//gameplay variables
+//GAMEPLAY VARIABLES
 const startingHandSize = 4;
 const startingBrawn = 4;
 const winningScore = 30;
 let userTurn = true; //boolean for whose turn it is. user = true, rival = false;
 
-//USER setup - "User" to be used in all places, no more "Player"
+//USER setup
 let userBones = 0;
-let userBrawn = 0;
+let userBrawn = startingBrawn;
 let userCheer = 0;
 let userFear = 0;
 
 //RIVAL setup
 let rivalBones = 0;
-let rivalBrawn = 0;
+let rivalBrawn = startingBrawn;
 let rivalCheer = 0;
 let rivalFear = 0;
 
@@ -33,7 +33,7 @@ CARD IDs:
 
 101 - <the Morrigan card 1>
 102 - <the Morrigan card 2> - raven? 
-103 - <the Morrigan card 3>
+103 - <the Morrigan card 3> - shapeshift - steal and play a card of your opponent's special deck
 104 - <Old Croghan card 1>
 105 - <Old Croghan card 2>
 106 - <Old Croghan card 3>
@@ -55,7 +55,7 @@ const basicDeck = [
         cardName: "Dredge",
         cardCost: 1,
         cardImg: "",
-        cardText: `Find ${calculateBones(2)} bones.`,
+        cardText: `Find ${calculateBones(2, userTurn)} bones.`,
         cardType: "basic",
         cardCopies: 3
     },
@@ -64,7 +64,7 @@ const basicDeck = [
         cardName: "Excavate",
         cardCost: 2,
         cardImg: "",
-        cardText: `Find ${calculateBones(4)} bones.`,
+        cardText: `Find ${calculateBones(4, userTurn)} bones.`,
         cardType: "basic",
         cardCopies: 2
     },
@@ -73,7 +73,7 @@ const basicDeck = [
         cardName: "Exhume",
         cardCost: 3,
         cardImg: "",
-        cardText: `Find ${calculateBones(7)} bones.`,
+        cardText: `Find ${calculateBones(7, userTurn)} bones.`,
         cardType: "basic",
         cardCopies: 1
     },
@@ -82,7 +82,7 @@ const basicDeck = [
         cardName: "Yoink",
         cardCost: 3,
         cardImg: "",
-        cardText: `Steal ${calculateBones(3)} bones.`,
+        cardText: `Steal ${calculateBones(3, userTurn)} bones.`,
         cardType: "basic",
         cardCopies: 1
     },
@@ -127,23 +127,23 @@ const basicDeck = [
         cardName: "Hearty Haul",
         cardCost: 2,
         cardImg: "",
-        cardText: `Find ${calculateBones(2)} bones, remove 1 fear.`,
+        cardText: `Find ${calculateBones(2, userTurn)} bones. \nRemove 1 fear.`,
         cardType: "basic",
         cardCopies: 1
     },
     {
-        cardId: 10, 
+        cardId: 10,
         cardName: "Gold Teeth",
         cardCost: 3,
         cardImg: "",
-        cardText: `Find ${calculateBones(3)} bones, gain 1 cheer.`,
+        cardText: `Find ${calculateBones(3, userTurn)} bones. \nGain 1 cheer.`,
         cardType: "basic",
         cardCopies: 1
     }
 ];
 
 //SPECIAL DECKS:
-//To be implemented once designed
+//To be implemented once finished being designed
 
 /*
 
@@ -275,6 +275,204 @@ const jackDeck = [
 
 //Game Setup
 
+//Building decks:
+let userDeck = []; //baseline deck to pull from
+let rivalDeck = [];
+fillDecks(); //fills both decks with basic cards
+let userRemainingDeck = Array.from(userDeck); //deck to manipulate each turn
+let rivalRemainingDeck = Array.from(rivalDeck);
+let userHand = []; //cards drawn/displayed
+let rivalHand = [];
+
+
+//GAME LOOP:
+//while (gameOver)
+//User Turn
+//Brawn refilled
+refillBrawn();
+//Draw cards
+drawCards();
+//display hand
+displayHand();
+
+//play cards
+//endTurn -> remove all card elements, flush hand, refill remaining deck
+//pass turn -> userTurn = false
+//rival brawn refilled
+//rival draws cards
+//rival plays cards
+//pass turn -> userTurn = true
+
+
+
+//logic to display a card on the gameboard
+function displayCard(card) {
+    //new card
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");
+    newCard.classList.add(`${card.cardType}`);
+    //card heading
+    let cardHeading = document.createElement("div");
+    cardHeading.classList.add("card-heading");
+    //two paragraphs within card heading
+    //card name with name inserted
+    let cardName = document.createElement("p");
+    cardName.classList.add("card-name");
+    cardName.innerText = `${card.cardName}`;
+    //card cost with cost inserted
+    let cardCost = document.createElement("p");
+    cardCost.classList.add("card-cost");
+    cardCost.innerText = `${card.cardCost}`;
+    //append paragraphs to card heading
+    cardHeading.appendChild(cardName);
+    cardHeading.appendChild(cardCost);
+
+    //card image
+    let cardImg = document.createElement("div");
+    cardImg.classList.add("card-img");
+    //TODO: make art lol
+
+    //card body
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    //add card text paragraph to body
+    let cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    cardText.innerText = `${card.cardText}`;
+    //append card text to card body
+    cardBody.appendChild(cardText);
+
+    //append header, img, and body to newCard
+    newCard.appendChild(cardHeading);
+    newCard.appendChild(cardImg);
+    newCard.appendChild(cardBody);
+
+    //add event listeners for clicking - one for card ability, one for card removal
+    newCard.addEventListener('click', (e) => {
+        if (checkCost(card.cardCost)){
+            playCard(card.cardId);
+            removeCard(e);
+        }
+    });
+
+    //append new card to gameboard
+    let gameBoard = document.getElementById("gameboard");
+    gameBoard.appendChild(newCard);
+}
+
+//check if card is playable
+function checkCost(cost){
+    if (userTurn){
+        if (cost > userBrawn){
+            return false; 
+        } else {
+            return true;
+        }
+    } else {
+        if (cost > rivalBrawn){
+            return false; 
+        } else {
+            return true;
+        }
+    }
+}
+
+function playCard(id) {
+console.log(id);
+
+}
+
+function removeCard(e) {
+    let element = e.currentTarget;
+    element.remove();
+}
+
+
+/*
+
+        let newCard = document.createElement("div");
+        newCard.classList.add("card");
+
+        //title div
+        let cardTitleDiv = document.createElement("div");
+        cardTitleDiv.classList.add("card-title");
+        let cardNamePara = document.createElement("p");
+        cardNamePara.innerText = `${cardPulled.cardTitle}`;
+
+        let cardCostPara = document.createElement("p");
+        cardCostPara.innerText = `${cardPulled.cardCost}`;
+        cardCostPara.classList.add("cost");
+        cardTitleDiv.appendChild(cardNamePara);
+        cardTitleDiv.appendChild(cardCostPara);
+
+        let cardImgDiv = document.createElement("div");
+        cardImgDiv.classList.add("card-img");
+
+        let cardTextDiv = document.createElement("div");
+        cardTextDiv.classList.add("card-text");
+        cardTextDiv.innerText = `${cardPulled.cardText}`;
+
+
+        newCard.appendChild(cardTitleDiv);
+        newCard.appendChild(cardImgDiv);
+        newCard.appendChild(cardTextDiv);
+
+        newCard.addEventListener('click', () => {playCard(cardPulled.cardTitle)});
+        newCard.addEventListener('click', (e) => {
+            if (checkCost(cardPulled.cardCost)) {
+                removeCard(e);
+            }
+        });
+        const gameBoard = document.getElementById("gameboard");
+        gameBoard.appendChild(newCard);
+        drawingCards.splice(draw, 1);
+    }
+*/
+
+
+//displays each card in hand
+function displayHand() {
+    for (i = 0; i < userHand.length; i++) {
+        displayCard(userHand[i]);
+    }
+}
+
+
+//working:
+//fill both decks from selected cards
+function fillDecks() {
+    for (i = 0; i < basicDeck.length; i++) {
+        let currentCard = basicDeck[i];
+        for (j = 0; j < currentCard.cardCopies; j++) {
+            userDeck.push(currentCard);
+            rivalDeck.push(currentCard);
+        }
+    }
+}
+
+//hand is drawn depending on whose turn it is 
+function drawCards() {
+    for (i = 0; i < startingHandSize; i++) {
+        if (userTurn) {
+            let draw = Math.floor(Math.random() * userRemainingDeck.length);
+            userHand.push(userRemainingDeck[draw]);
+            userRemainingDeck.splice(draw, 1);
+        } else {
+            let draw = Math.floor(Math.random() * rivalRemainingDeck.length);
+            rivalHand.push(rivalRemainingDeck[draw]);
+            rivalRemainingDeck.splice(draw, 1);
+        }
+    }
+}
+
+//refills brawn depending on whose turn it is
+function refillBrawn() {
+    if (userTurn) {
+        userBrawn = startingBrawn;
+    } else {
+        rivalBrawn = startingBrawn;
+    }
+}
 
 
 
@@ -282,6 +480,48 @@ const jackDeck = [
 
 
 
+//returns false until a score reaching winningScore
+function gameOver() {
+    if (userBones >= winningScore || rivalBones >= winningScore) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function calculateBones(num, player) {
+    let bones = num;
+
+    if (player) {
+        bones += userCheer;
+        bones -= userFear;
+        if (bones < 0) {
+            bones = 0;
+        }
+        return bones;
+    } else {
+        bones += rivalCheer;
+        bones -= rivalFear;
+        if (bones < 0) {
+            bones = 0;
+        }
+        return bones;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
 
 
 
@@ -376,11 +616,6 @@ function rivalTurn() {
 }
 
 
-function removeCard(e) {
-    let element = e.currentTarget;
-    e
-    element.remove();
-}
 
 //Player Card Functions:
 
@@ -707,17 +942,17 @@ let tempArr = [
         cardCost: 2,
         cardText: `Gain ${4+userHype-userFear} points`,
         cardFunc: gavotte,
-        cardImg: ""   
+        cardImg: ""
     },
     {
         cardTitle: "One Step Pep",
         cardCost: 2,
         cardText: `Gain 1 Hype`,
         cardFunc: pep,
-        cardImg: ""   
+        cardImg: ""
     },
 
-    
+
 ];
 userBeats = 4;
 drawingCards = Array.from(tempArr);
@@ -747,7 +982,7 @@ drawingCards = Array.from(tempArr);
         let cardTextDiv = document.createElement("div");
         cardTextDiv.classList.add("card-text");
         cardTextDiv.innerText = `${cardPulled.cardText}`;
-        
+
 
         newCard.appendChild(cardTitleDiv);
         newCard.appendChild(cardImgDiv);
@@ -774,3 +1009,5 @@ function updatePlayerScore(points){
     playerScore.innerText = `${userPoints}`;
     playerScoreBar.setAttribute('value', `${userPoints}`);
 }
+
+*/
